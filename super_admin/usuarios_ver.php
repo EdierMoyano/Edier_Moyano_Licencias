964394c6a8c00fa91bdb183f3d560f323bd3d55c
EdierMoyano <ediersmb@gmail.com>
2025-04-25 15:52:03 -0500

    cerrar php

--- super_admin/usuarios_ver.php
+++ super_admin/usuarios_ver.php
@@ -170,7 +170,7 @@ try {
                 </div>
             </div>
             
-          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">Detalles de Usuario</h1>
                     <div class="btn-toolbar mb-2 mb-md-0">
@@ -360,3 +360,5 @@ try {
     </script>
 </body>
 </html>
+
+<?php
